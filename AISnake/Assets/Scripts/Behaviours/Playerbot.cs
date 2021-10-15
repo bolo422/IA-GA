cf656c60149907df14080e42d38fe37ec44cc39d
Erick Hasse (Bolo) <50816767+bolo422@users.noreply.github.com>
2021-10-14 23:26:20 -0300

    bot assassino

--- AISnake/Assets/Scripts/Behaviours/Playerbot.cs
+++ AISnake/Assets/Scripts/Behaviours/Playerbot.cs
@@ -8,22 +8,24 @@ public class Playerbot : AIBehaviour
     public Vector3 waypoint = new Vector3(0, 0, 0);
 
     public float detectionRadius = 100f;
-    public string tagEnemyBoy = "bot";
-    public string tagComidas = "Orb";
+    public string botTag = "Bot";
+    public string bodyTag = "Body";
+    public string orbTag = "Orb";
+
+    bool encontrouInimigo = false;
 
 
 
     public override void Init(GameObject own, SnakeMovement ownMove)
     {
         base.Init(own, ownMove);
-        ownerMovement.StartCoroutine(UpdateDirEveryXSeconds(timeChangeDir));
+        ownerMovement.StartCoroutine(UpdateDirEveryXSeconds(0));
     }
 
     //seria interessante ter um controlador com o colisor que define o mundo pra poder gerar pontos dentro desse colisor
 
     public override void Execute()
     {
-
         MoveForward();
     }
 
@@ -32,6 +34,7 @@ public class Playerbot : AIBehaviour
     {
         
         owner.transform.position = Vector2.MoveTowards(owner.transform.position, waypoint, ownerMovement.speed * Time.deltaTime);
+        
     }
 
 
@@ -41,40 +44,115 @@ public class Playerbot : AIBehaviour
         ownerMovement.StopCoroutine(UpdateDirEveryXSeconds(1));
         //Collider[] hitColliders = Physics.OverlapSphere(owner.transform.position, detectionRadius);
         Collider2D[] hitColliders = Physics2D.OverlapCircleAll(owner.transform.position, detectionRadius);
-        List<Vector2> comidas = new List<Vector2>();
+        List<Vector2> orbs = new List<Vector2>();
         List<Vector2> bots = new List<Vector2>();
+        List<Vector2> bodys = new List<Vector2>();
 
 
-        float menorDistancia = detectionRadius * 10;
         // float menorDistanciaPos = -1;
 
       
         foreach (var hitCollider in hitColliders)
         {
-            if (hitCollider.CompareTag(tagComidas))
+            if (hitCollider.CompareTag(orbTag))
             {
-                comidas.Add(hitCollider.GetComponent<Transform>().position);
-                Debug.Log("colisão");
-
+                orbs.Add(hitCollider.GetComponent<Transform>().position);
+            }
+            if (hitCollider.CompareTag(botTag))
+            {
+                bots.Add(hitCollider.GetComponent<Transform>().position);
+            }
+            if (hitCollider.CompareTag(bodyTag))
+            {
+                bodys.Add(hitCollider.GetComponent<Transform>().position);
             }
         }
 
-        if (bots.Count < 1)
+        if (bots.Count < 1 && orbs.Count > 0)
         {
-            Vector2[] comidasArray = comidas.ToArray();
+            Vector2[] orbsArray = orbs.ToArray();
+            float menorDistancia = detectionRadius * 10;
 
-
-
-            for (int i = 0; i < comidasArray.Length; i++)
+            for (int i = 0; i < orbsArray.Length; i++)
             {
-                if(Vector2.Distance(owner.transform.position, comidasArray[i]) < menorDistancia)
+                if(Vector2.Distance(owner.transform.position, orbsArray[i]) < menorDistancia)
                 {
-                    menorDistancia = Vector2.Distance(owner.transform.position, comidasArray[i]);
-
-                    waypoint = comidasArray[i];
-                    //enorDistanciaPos = -1;
+                    menorDistancia = Vector2.Distance(owner.transform.position, orbsArray[i]);
+                    waypoint = orbsArray[i];
                 }
             }
+        }
+        //// a partir daqui está em desenvolvimento, se usar oq tem aicmas já funciona para seguir as comidas
+
+        else
+        {
+            //Estado teste = Matar a cobrinha mais próxima, se ela estiver mais próxima q uma comida
+            if(orbs.Count > 0)
+            {
+                Vector2[] orbsArray = orbs.ToArray();
+                float menorDistanciaOrbs = detectionRadius * 10;
+
+                for (int i = 0; i < orbsArray.Length; i++)
+                {
+                    if (Vector2.Distance(owner.transform.position, orbsArray[i]) < menorDistanciaOrbs)
+                    {
+                        menorDistanciaOrbs = Vector2.Distance(owner.transform.position, orbsArray[i]);
+                        waypoint = orbsArray[i];
+
+                    }
+                }
+                
+                Vector2[] botsArray = bots.ToArray();
+                float menorDistanciaBots = detectionRadius * 10;
+                Vector2 botComMenorDistancia = new Vector2();
+
+                for (int i = 0; i < botsArray.Length; i++)
+                {
+                    if (Vector2.Distance(owner.transform.position, botsArray[i]) < menorDistanciaBots && Vector2.Distance(owner.transform.position, botsArray[i]) > 0)
+                    {
+                        menorDistanciaBots = Vector2.Distance(owner.transform.position, botsArray[i]);
+                        botComMenorDistancia = botsArray[i];
+                    }
+                }
+
+                if(menorDistanciaOrbs >= menorDistanciaBots)
+                //if(true)
+                {
+                    Vector2[] bodysArray = bodys.ToArray();
+                    float menorDistanciaBodyToBot = detectionRadius * 10;
+                    Vector2 bodyComMenorDistancia = new Vector2(); ;
+
+                    for (int i = 0; i < bodysArray.Length; i++)
+                    {
+                        float bodyToBot = Vector2.Distance(bodysArray[i], botComMenorDistancia);
+
+                        if (bodyToBot < menorDistanciaBodyToBot)
+                        {
+                            menorDistanciaBodyToBot = bodyToBot;
+                            bodyComMenorDistancia = bodysArray[i];
+                        }
+                    }
+
+                    Vector2 pf = (botComMenorDistancia - bodyComMenorDistancia) + botComMenorDistancia;
+
+                    float mult = 1.5f;
+                    pf = new Vector2(pf.x + mult, pf.y + mult);
+                    waypoint = pf;
+
+
+                }
+                else
+                {
+                    // vazio, pq o waypoint já está definido no orb mais próximo
+                }
+
+
+
+            }
+
+
+
+
         }
 
 
@@ -83,6 +161,9 @@ public class Playerbot : AIBehaviour
 
         ownerMovement.StartCoroutine(UpdateDirEveryXSeconds(x));
     }
+
+
+
 
 
 
